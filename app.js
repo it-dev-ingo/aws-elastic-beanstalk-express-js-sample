609bd43db1d429d8fd7e136623924261e718dfc3
Ingo Henrique <ingo.girao@wildlifestudios.com>
2021-04-08 04:34:15 +0000

    mario

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('MELHOR TESTE DE PIPELINE DO MUNDO'));
+app.get('/', (req, res) => res.send('teste do mário'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
